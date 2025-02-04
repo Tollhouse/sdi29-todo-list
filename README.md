613d4d91c825c7ef94dce3457a1109805ad67ea8
Tollhouse <mistertollhouse@gmail.com>
2025-02-04 15:00:21 -0700

    Download button working with saved data in local storage.

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Simple site that uses an input field and a couple of buttons to generate and dis
 Enter text into the text input box.\
 Click on the submit button.\
 Save button puts currently displayed todo items into local storage.\
-Load button takes the current local storage and resubmits it to the display.\
+Load button takes the current local storage and resubmits it to the display.
--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var text = "";
 var display_list = document.getElementById("list");
 var submit_text = document.getElementById("submit-field");
 var button = document.getElementById("submit-button");
-var save = document.getElementById("save")
-var load = document.getElementById("load")
+var save = document.getElementById("save_button");
+var load = document.getElementById("load_button");
+var download_button = document.getElementById("download_button");
 
 button.addEventListener("click",() => {
     if(submit_text.value !== ''){
@@ -23,12 +24,33 @@ save.addEventListener("click",() => {
 load.addEventListener("click",() => {
     var stored = localStorage.getItem("total_list");
     if(stored){
-        var word_list = stored.split(",")
+        var word_list = stored.split(",");
         word_list.forEach(function(item){
             child = document.createElement('p');
             child.textContent = submit_text.value;
             display_list.appendChild(child);
-        })
+        });
     }
-    console.log(stored)
-})
+});
+
+download_button.addEventListener("click",() => {
+    var stored = localStorage.getItem("total_list");
+    var count = 1;
+    var output = [];
+    if(stored){
+        var word_list = stored.split(",");
+        word_list.forEach(function(item){
+            output += count + '. ' + item + '\n';
+            count++;
+        });
+        var blob = new Blob([output], {type: 'text/plain'});
+        var out = document.createElement('a');
+        if(out){
+            out.href = URL.createObjectURL(blob);
+            out.download = "todo.txt";
+            out.click('out');
+            URL.revokeObjectURL(out.href);
+        }
+
+    }
+});
