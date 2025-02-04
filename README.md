4f2b6a1435d702e6f064d2cb8a457eb3a2930e84
Tollhouse <mistertollhouse@gmail.com>
2025-02-04 11:49:47 -0700

    Save and load functionality added to the buttons

---
+++ README.md
@@ -0,0 +1,10 @@
+# This is a to-do list maker website
+
+Simple site that uses an input field and a couple of buttons to generate and display a list of text
+
+**Usage**
+
+Enter text into the text input box.
+Click on the submit button.
+Save button puts currently displayed todo items into local storage.
+Load button takes the current local storage and resubmits it to the display.
--- index.js
+++ index.js
@@ -5,16 +5,30 @@ var display_list = document.getElementById("list");
 var submit_text = document.getElementById("submit-field");
 var button = document.getElementById("submit-button");
 var save = document.getElementById("save")
+var load = document.getElementById("load")
 
 button.addEventListener("click",() => {
-    todo_items.push(submit_text.value);
-    child = document.createElement('p');
-    child.textContent = submit_text.value;
-    display_list.appendChild(child);
+    if(submit_text.value !== ''){
+        todo_items.push(submit_text.value);
+        child = document.createElement('p');
+        child.textContent = submit_text.value;
+        display_list.appendChild(child);
+        }
 });
 
-save.addEventListener("click",() =>{
-    todo_items.forEach(function(item){
-        console.log(item)
-    })
+save.addEventListener("click",() => {
+    localStorage.setItem("total_list",todo_items);
 });
+
+load.addEventListener("click",() => {
+    var stored = localStorage.getItem("total_list");
+    if(stored){
+        var word_list = stored.split(",")
+        word_list.forEach(function(item){
+            child = document.createElement('p');
+            child.textContent = submit_text.value;
+            display_list.appendChild(child);
+        })
+    }
+    console.log(stored)
+})
